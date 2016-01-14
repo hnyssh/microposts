b76b3f0ee84d9c7774f11d573055106292d6c7ca
Yasushi Ohno <hnyssh@gmail.com>
2016-01-14 04:02:30 +0000

    add profile and area

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+
+  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :current_user?, only:[:edit, :update]
 
   def show
    @user = User.find(params[:id])
@@ -18,13 +22,34 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'ユーザーを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
 
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :profile, :area)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def current_user?
+    if @user != current_user
+      redirect_to root_path, alert: "不正なアクセス"
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :profile, length: { maximum: 1024 }
+  validates :profile, presence: true, on: :update  # 更新時のみチェック
+  validates :area, length: { maximum: 30 }
+  validates :area, presence: true, on: :update  # 更新時のみチェック
+
   has_secure_password
 end
